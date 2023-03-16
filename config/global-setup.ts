587de33f95d629aa24a4da979ca86d0746604a44
Michael Lee <michaelleemichaellee408@gmail.com>
2023-03-16 15:30:43 +0100

    Change storage state path

--- config/global-setup.ts
+++ config/global-setup.ts
@@ -16,7 +16,7 @@ async function globalSetup() {
   await page_utils.wp_admin_login();
 
   // Save signed-in state to 'storageState.json'.
-  await page.context().storageState({ path: 'tests/e2e/config/storageState.json' });
+  await page.context().storageState({ path: './config/storageState.json' });
   await browser.close();
 }
 
