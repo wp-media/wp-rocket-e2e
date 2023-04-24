4712d98ec212a14803342a0d7fa50329d8747e8f
Michael Lee <michaelleemichaellee408@gmail.com>
2023-04-24 12:57:55 +0100

    Updated config

--- config/wp.config.ts
+++ config/wp.config.ts
@@ -1,12 +1,14 @@
 const WP_ADMIN_USER = {
-	username: 'admin',
-	password: 'password',
+	username: 'live_username',
+	password: 'live_password',
+	local_username: 'admin',
+	local_password: 'password'
 } as const;
 
 const {
-	WP_USERNAME = WP_ADMIN_USER.username,
-	WP_PASSWORD = WP_ADMIN_USER.password,
-	WP_BASE_URL = 'http://localhost:8888',
+	WP_USERNAME = process.env.CI ? WP_ADMIN_USER.local_username : WP_ADMIN_USER.username,
+	WP_PASSWORD = process.env.CI ? WP_ADMIN_USER.local_password : WP_ADMIN_USER.password,
+	WP_BASE_URL = 'https://example.org'
 } = process.env;
 
-export { WP_ADMIN_USER, WP_USERNAME, WP_PASSWORD, WP_BASE_URL };
+export { WP_USERNAME, WP_PASSWORD, WP_BASE_URL };
