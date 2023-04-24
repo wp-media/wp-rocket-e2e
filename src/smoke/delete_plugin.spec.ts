03066c0e5f0d36109360ea23970f9dd715ff90dd
Michael Lee <michaelleemichaellee408@gmail.com>
2023-04-24 23:46:17 +0100

    Added first smoke test

---
+++ src/smoke/delete_plugin.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from '@playwright/test';
+
+/**
+ * Local deps.
+ */
+import { pageUtils } from '../../utils/page.utils';
+
+const deletePlugin = () => {
+    test('Should successfully delete the plugin', async ( { page } ) => {
+    
+        const page_utils = new pageUtils( page );
+ 
+        await page_utils.visit_page('wp-admin');
+        await page_utils.goto_plugin();
+
+        const locator = {
+            'delete': page.locator( '#delete-wp-rocket' )
+        };
+
+        // With WPR deactivated, Delete WPR
+        page_utils.toggle_plugin_activation('wp-rocket', false);
+        await locator.delete.click();
+
+        // Check that WPR is deactivated
+        await expect(page.locator('#activate-wp-rocket')).toBeHidden();
+        // Check that there is no related error in debug.log
+        await expect(page.locator('#wpr_debug_log_notice')).toBeHidden();
+
+        // Upload WPR Zip Archive and Activate
+        await page_utils.upload_new_plugin('../plugin/wp-rocket.zip');
+        // Enable all settings and save, then deactivate.
+    });
+}
+
+export default deletePlugin;
