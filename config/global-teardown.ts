e9c6f8767489023e5cef7e037e43309ef13f87c7
Michael Lee <michaelleemichaellee408@gmail.com>
2023-04-24 17:45:32 +0100

    Updated global teardown

--- config/global-teardown.ts
+++ config/global-teardown.ts
@@ -1,14 +1,4 @@
 async function globalTeardown() {
-  let command: String;
-
-  command = 'npm run wp-env run cli wp db reset -- --yes\n';
-  command += 'npm run wp-env run cli wp core install -- --url=localhost:8888 --title=wp-rocket --admin_user=admin --admin_password=password --admin_email=admin@test.com\n';
-  command += 'npm run wp-env stop';
-    
-  // Run teardown commands.
-  const {execSync} = require('child_process');
-  execSync(command);
-
   // Delete 'storageState.json'.
   const fs = require('fs');
   fs.unlink('./config/storageState.json', err => {
