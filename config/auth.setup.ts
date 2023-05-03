a6641403979c919975a7560cd7726c88092e81f8
Michael Lee <michaelleemichaellee408@gmail.com>
2023-05-03 14:00:56 +0100

    Use new auth method

--- config/auth.setup.ts
+++ config/auth.setup.ts
@@ -11,14 +11,5 @@ const authFile = './config/storageState.json';
 setup('authenticate', async ({ page }) => {
   const page_utils = new pageUtils( page );
 
-  await page_utils.visit_page('wp-admin');
-  await page_utils.wp_admin_login();
-  // Wait until the page receives the cookies.
-  //
-  // Sometimes login flow sets cookies in the process of several redirects.
-  // Wait for the final URL to ensure that the cookies are actually set.
-  await page.waitForURL(WP_BASE_URL + '/wp-admin/');
-
-  // End of authentication steps.
-  await page.context().storageState({ path: authFile });
+  await page_utils.auth();
 });
