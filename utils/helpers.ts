553b4834d71a58afa2be20e00a0f5d71adbe54da
Michael Lee <michaelleemichaellee408@gmail.com>
2023-05-04 15:54:38 +0100

    New function for checking that settings is exported correctly

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,6 +1,9 @@
 import os from 'os';
 import fs from 'fs/promises';
 
+// Interfaces
+import { exportedSettings } from './interfaces';
+
 let home_dir: String, install_path: String;
 home_dir = os.homedir();
 
@@ -93,3 +96,67 @@ export const is_rocket_active = async () => {
         return false;
     }
 }
+
+/**
+ * Read file content
+ */
+export const read_any_file = async (file) => {
+    return await fs.readFile(file, 'utf8');
+}
+
+/**
+ * Check that settings is exported correctly.
+ *
+ * @param   {exportedSettings}  exported_settings  Object of exported settings.
+ * @param   {string}            exception          Object key to exclude from check.
+ *
+ * @return  {Promise<boolean>}                     Return bool.
+ */
+export const is_exported_correctly = async (exported_settings: exportedSettings, exception: string): Promise< boolean > => {
+    const ui_reflected_settings = [
+        'lazyload',
+        'remove_unused_css',
+        'async_css',
+        'cache_logged_user',
+        'cache_mobile',
+        'do_caching_mobile_files',
+        'minify_css',
+        'minify_js',
+        'minify_concatenate_css',
+        'minify_concatenate_js',
+        'defer_all_js',
+        'lazyload_iframes',
+        'lazyload_youtube',
+        'database_revisions',
+        'database_auto_drafts',
+        'database_trashed_posts',
+        'database_spam_comments',
+        'database_trashed_comments',
+        'database_all_transients',
+        'database_optimize_tables',
+        'schedule_automatic_cleanup',
+        'manual_preload',
+        'do_cloudflare',
+        'sucury_waf_cache_sync',
+        'control_heartbeat',
+        'cdn',
+        'varnish_auto_purge',
+        'image_dimensions',
+        'delay_js', 
+    ];
+
+    for (let key in exported_settings) {
+        for (let option of ui_reflected_settings) {
+            if (key == exception) {
+                continue;
+            }
+
+            if (key === option && exported_settings[key] !== 0) {
+                return false;
+            }
+        }
+     }
+
+     return true;
+}
+
