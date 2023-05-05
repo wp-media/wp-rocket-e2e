799cb55e46b10f125e01519c49dd31a5e7ced5f9
Michael Lee <michaelleemichaellee408@gmail.com>
2023-05-05 13:42:13 +0100

    Added exclusions

---
+++ utils/exclusions.ts
@@ -0,0 +1,38 @@
+export const ui_reflected_settings = [
+    'lazyload',
+    'remove_unused_css',
+    'async_css',
+    'cache_logged_user',
+    'cache_mobile',
+    'do_caching_mobile_files',
+    'minify_css',
+    'minify_js',
+    'minify_concatenate_css',
+    'minify_concatenate_js',
+    'defer_all_js',
+    'lazyload_iframes',
+    'lazyload_youtube',
+    'database_revisions',
+    'database_auto_drafts',
+    'database_trashed_posts',
+    'database_spam_comments',
+    'database_trashed_comments',
+    'database_all_transients',
+    'database_optimize_tables',
+    'schedule_automatic_cleanup',
+    'manual_preload',
+    'do_cloudflare',
+    'sucury_waf_cache_sync',
+    'control_heartbeat',
+    'cdn',
+    'varnish_auto_purge',
+    'image_dimensions',
+    'delay_js', 
+];
+
+export const diff_checker = [
+    "\"version\":",
+    "\"delay_js\":",
+    "\"delay_js_exclusions_selected\":",
+    "\"delay_js_exclusions_selected_exclusions\":"
+];
