216eeb7d10c998f19ba337de48dba6b5f143f954
Michael Lee <michaelleemichaellee408@gmail.com>
2023-07-27 20:59:57 +0100

    Added new fixture

--- src/common/fixtures.ts
+++ src/common/fixtures.ts
@@ -1,9 +1,11 @@
 import { test as base } from '@playwright/test';
 import { Sections } from './sections';
 import { selectors as pluginSelectors } from "./selectors";
+import { PageUtils } from '../../utils/page-utils';
 
 type WprFixtures = {
   sections: Sections;
+  utils: PageUtils;
 };
 
 export const test = base.extend<WprFixtures>({
@@ -12,6 +14,11 @@ export const test = base.extend<WprFixtures>({
     const sections = new Sections(page, pluginSelectors);
     await use(sections);
   },
+  utils: async ({ page }, use) => {
+    // Set up the fixture.
+    const utils = new PageUtils(page, new Sections(page, pluginSelectors));
+    await use(utils);
+  }
 });
 
 export { expect } from '@playwright/test';
