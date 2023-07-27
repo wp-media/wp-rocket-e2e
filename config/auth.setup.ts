c411041419cc41d9eb6d646601b56ae711fadf69
Michael Lee <michaelleemichaellee408@gmail.com>
2023-07-27 21:40:05 +0100

    Use util fixture

--- config/auth.setup.ts
+++ config/auth.setup.ts
@@ -1,15 +1,5 @@
-import { test as setup } from '@playwright/test';
+import { test as setup } from '../src/common/fixtures';
 
-/**
- * Local deps.
- */
-import { WP_BASE_URL } from '../config/wp.config';
-import { pageUtils } from '../utils/page.utils';
-
-const authFile = './config/storageState.json';
-
-setup('authenticate', async ({ page }) => {
-  const page_utils = new pageUtils( page );
-
-  await page_utils.auth();
+setup('authenticate', async ({ utils }) => {
+  await utils.auth();
 });
