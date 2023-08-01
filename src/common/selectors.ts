7ec1ca5b25bc5e4db105b0fb3c084af90c6b809a
Michael Lee <michaelleemichaellee408@gmail.com>
2023-08-01 10:36:25 +0100

    Updated data structure

--- src/common/selectors.ts
+++ src/common/selectors.ts
@@ -1,15 +1,14 @@
 import type { Page } from "@playwright/test";
 import { isElementEnabled, activateFromPopUp } from "../../utils/helpers";
 import type { Selectors } from "../../utils/types";
+import { FieldType } from "../../utils/types";
 
 export const selectors: Selectors = {
     dashboard: {
         parent: "dashboard",
         elements: {
             clearCacheBtn: {
-                button: {
-                    target: "div[role=\"main\"] >> text=Clear and preload cache",
-                }
+                type: FieldType.textbox,
             }
         }
     },
@@ -17,22 +16,19 @@ export const selectors: Selectors = {
         parent: "cache",
         elements: {
             mobileDeviceCache: {
-                checkbox: {
-                    element: "#cache_mobile",
-                    target: "label[for=cache_mobile]"
-                }
+                type: FieldType.checkbox,
+                element: "#cache_mobile",
+                target: "label[for=cache_mobile]"
             },
             mobileDeviceSeparateCache: {
-                checkbox: {
-                    element: "#do_caching_mobile_files",
-                    target: "label[for=do_caching_mobile_files]"
-                }
+                type: FieldType.checkbox,
+                element: "#do_caching_mobile_files",
+                target: "label[for=do_caching_mobile_files]"
             },
             cacheLoggedUser: {
-                checkbox: {
-                    element: "#cache_logged_user",
-                    target: "label[for=cache_logged_user]"
-                }
+                type: FieldType.checkbox,
+                element: "#cache_logged_user",
+                target: "label[for=cache_logged_user]"
             }
         }
     },
@@ -40,29 +36,26 @@ export const selectors: Selectors = {
         parent: "file_optimization",
         elements: {
             minifyCss: {
-                checkbox: {
-                    element: "#minify_css",
-                    target: "label[for=minify_css]",
-                },
+                type: FieldType.checkbox,
+                element: "#minify_css",
+                target: "label[for=minify_css]",
                 after: async (page: Page, state: boolean): Promise<void> => {
                      activateFromPopUp(page, state, "text=Activate minify CSS") 
                 }
             },
             combineCss: {
-                checkbox: {
-                    element: "#minify_concatenate_css",
-                    target: "label[for=minify_concatenate_css]",
-                },
+                type: FieldType.checkbox,
+                element: "#minify_concatenate_css",
+                target: "label[for=minify_concatenate_css]",
                 before: async (page: Page): Promise<boolean> => { return isElementEnabled(page, "#minify_concatenate_css") },
                 after: async (page: Page, state: boolean): Promise<void> => {
                     activateFromPopUp(page, state, "text=Activate combine CSS") 
                }
             },
             rucss:{
-                checkbox: {
-                    element: "#optimize_css_delivery",
-                    target: "label[for=optimize_css_delivery]",
-                },
+                type: FieldType.checkbox,
+                element: "#optimize_css_delivery",
+                target: "label[for=optimize_css_delivery]",
                 after: async (page: Page): Promise<void> => {
                     if (await page.locator("text=Activate Remove Unused CSS").isHidden()) {
                         return;
@@ -72,35 +65,31 @@ export const selectors: Selectors = {
                 }
             },
             minifyJs: {
-                checkbox: {
-                    element: "#minify_js",
-                    target: "label[for=minify_js]",
-                },
+                type: FieldType.checkbox,
+                element: "#minify_js",
+                target: "label[for=minify_js]",
                 after: async (page: Page, state: boolean): Promise<void> => {
                     activateFromPopUp(page, state, "Activate minify JavaScript") 
                }
             },
             combineJs: {
-                checkbox: {
-                    element: "#minify_concatenate_js",
-                    target: "label[for=minify_concatenate_js]",
-                },
+                type: FieldType.checkbox,
+                element: "#minify_concatenate_js",
+                target: "label[for=minify_concatenate_js]",
                 before: async (page: Page): Promise<boolean> => { return isElementEnabled(page, "#minify_concatenate_js") },
                 after: async (page: Page, state: boolean): Promise<void> => {
                     activateFromPopUp(page, state, "Activate combine JavaScript") 
                }
             },
             deferJs: {
-                checkbox: {
-                    element: "#defer_all_js",
-                    target: "label[for=defer_all_js]"
-                }
+                type: FieldType.checkbox,
+                element: "#defer_all_js",
+                target: "label[for=defer_all_js]"
             },
             delayJs: {
-                checkbox: {
-                    element: "#delay_js",
-                    target: "label[for=delay_js]"
-                }
+                type: FieldType.checkbox,
+                element: "#delay_js",
+                target: "label[for=delay_js]"
             }
         }
     },
@@ -108,31 +97,27 @@ export const selectors: Selectors = {
         parent: "media",
         elements: {
             lazyload: {
-                checkbox: {
-                    element: "#lazyload",
-                    target: "label[for=lazyload]"
-                }
+                type: FieldType.checkbox,
+                element: "#lazyload",
+                target: "label[for=lazyload]"
             },
             lazyloadIframes: {
-                checkbox: {
-                    element: "#lazyload_iframes",
-                    target: "label[for=lazyload_iframes]"
-                }
+                type: FieldType.checkbox,
+                element: "#lazyload_iframes",
+                target: "label[for=lazyload_iframes]"
             },
             lazyloadYoutube: {
-                checkbox: {
-                    element: "#lazyload_youtube",
-                    target: "label[for=lazyload_youtube]"
-                },
+                type: FieldType.checkbox,
+                element: "#lazyload_youtube",
+                target: "label[for=lazyload_youtube]",
                 before: async (page: Page): Promise<boolean> => {
                     return !await page.isHidden("#lazyload_youtube");
                 },
             },
             imageDimensions: {
-                checkbox: {
-                    element: "#image_dimensions",
-                    target: "label[for=image_dimensions]"
-                }
+                type: FieldType.checkbox,
+                element: "#image_dimensions",
+                target: "label[for=image_dimensions]"
             }
         }
     },
@@ -140,16 +125,14 @@ export const selectors: Selectors = {
         parent: "preload",
         elements: {
             preload: {
-                checkbox: {
-                    element: "#manual_preload",
-                    target: "label[for=manual_preload]"
-                }
+                type: FieldType.checkbox,
+                element: "#manual_preload",
+                target: "label[for=manual_preload]"
             },
             preloadLinks: {
-                checkbox: {
-                    element: "#preload_links",
-                    target: "label[for=preload_links]"
-                }
+                type: FieldType.checkbox,
+                element: "#preload_links",
+                target: "label[for=preload_links]"
             }
         }
     },
@@ -157,29 +140,24 @@ export const selectors: Selectors = {
         parent: "advanced_cache",
         elements: {
             cacheRejectUri: {
-                textbox: {
-                    element: "#cache_reject_uri"
-                }
+                type: FieldType.textbox,
+                element: "#cache_reject_uri"
             },
             cacheRejectCookies: {
-                textbox: {
-                    element: "#cache_reject_cookies"
-                }
+                type: FieldType.textbox,
+                element: "#cache_reject_cookies"
             },
             cacheRejectUa: {
-                textbox: {
-                    element: "#cache_reject_ua"
-                }
+                type: FieldType.textbox,
+                element: "#cache_reject_ua"
             },
             cachePurgePages: {
-                textbox: {
-                    element: "#cache_purge_pages"
-                }
+                type: FieldType.textbox,
+                element: "#cache_purge_pages"
             },
             cacheQueryStrings: {
-                textbox: {
-                    element: "#cache_query_strings"
-                }
+                type: FieldType.textbox,
+                element: "#cache_query_strings"
             },
         }
     },
@@ -187,52 +165,44 @@ export const selectors: Selectors = {
         parent: "database",
         elements: {
             databaseRevisions: {
-                checkbox: {
-                    element: "#database_revisions",
-                    target: "label[for=database_revisions]"
-                }
+                type: FieldType.checkbox,
+                element: "#database_revisions",
+                target: "label[for=database_revisions]"
             },
             databaseAutoDrafts: {
-                checkbox: {
-                    element: "#database_auto_drafts",
-                    target: "label[for=database_auto_drafts]"
-                }
+                type: FieldType.checkbox,
+                element: "#database_auto_drafts",
+                target: "label[for=database_auto_drafts]"
             },
             databaseTrashedPosts: {
-                checkbox: {
-                    element: "#database_trashed_posts",
-                    target: "label[for=database_trashed_posts]"
-                }
+                type: FieldType.checkbox,
+                element: "#database_trashed_posts",
+                target: "label[for=database_trashed_posts]"
             },
             databaseSpamComments: {
-                checkbox: {
-                    element: "#database_spam_comments",
-                    target: "label[for=database_spam_comments]"
-                }
+                type: FieldType.checkbox,
+                element: "#database_spam_comments",
+                target: "label[for=database_spam_comments]"
             },
             databaseTrashedComments: {
-                checkbox: {
-                    element: "#database_trashed_comments",
-                    target: "label[for=database_trashed_comments]"
-                }
+                type: FieldType.checkbox,
+                element: "#database_trashed_comments",
+                target: "label[for=database_trashed_comments]"
             },
             databaseAllTransients: {
-                checkbox: {
-                    element: "#database_all_transients",
-                    target: "label[for=database_all_transients]"
-                }
+                type: FieldType.checkbox,
+                element: "#database_all_transients",
+                target: "label[for=database_all_transients]"
             },
             databaseOptimizeTables: {
-                checkbox: {
-                    element: "#database_optimize_tables",
-                    target: "label[for=database_optimize_tables]"
-                }
+                type: FieldType.checkbox,
+                element: "#database_optimize_tables",
+                target: "label[for=database_optimize_tables]"
             },
             scheduleAutomaticCleanup: {
-                checkbox: {
-                    element: "#schedule_automatic_cleanup",
-                    target: "label[for=schedule_automatic_cleanup]"
-                }
+                type: FieldType.checkbox,
+                element: "#schedule_automatic_cleanup",
+                target: "label[for=schedule_automatic_cleanup]"
             }
         }
     },
@@ -240,16 +210,14 @@ export const selectors: Selectors = {
         parent: "page_cdn",
         elements: {
             cdn: {
-                checkbox: {
-                    element: "#cdn",
-                    target: "label[for=cdn]"
-                }
+                type: FieldType.checkbox,
+                element: "#cdn",
+                target: "label[for=cdn]"
             },
             cnames: {
-                role: {
-                    name: "textbox",
-                    roleTarget: { name: "cdn.example.com" }
-                }
+                type: FieldType.role,
+                name: "textbox",
+                roleTarget: { name: "cdn.example.com" }
             }
         }
     },
@@ -257,10 +225,9 @@ export const selectors: Selectors = {
         parent: "heartbeat",
         elements: {
             controlHeartbeat: {
-                checkbox: {
-                    element: "#control_heartbeat",
-                    target: "label[for=control_heartbeat]"
-                }
+                type: FieldType.checkbox,
+                element: "#control_heartbeat",
+                target: "label[for=control_heartbeat]"
             }
         }
     },
@@ -268,28 +235,24 @@ export const selectors: Selectors = {
         parent: "addons",
         elements: {
             varnishAutoPurge: {
-                checkbox: {
-                    element: "#varnish_auto_purge",
-                    target: "label[for=varnish_auto_purge]"
-                }
+                type: FieldType.checkbox,
+                element: "#varnish_auto_purge",
+                target: "label[for=varnish_auto_purge]"
             },
             cacheWebp: {
-                checkbox: {
-                    element: "#cache_webp",
-                    target: "label[for=cache_webp]"
-                }
+                type: FieldType.checkbox,
+                element: "#cache_webp",
+                target: "label[for=cache_webp]"
             },
             doCloudflare: {
-                checkbox: {
-                    element: "#do_cloudflare",
-                    target: "label[for=do_cloudflare]"
-                }
+                type: FieldType.checkbox,
+                element: "#do_cloudflare",
+                target: "label[for=do_cloudflare]"
             },
             sucuryWafCacheSync: {
-                checkbox: {
-                    element: "#sucury_waf_cache_sync",
-                    target: "label[for=sucury_waf_cache_sync]"
-                }
+                type: FieldType.checkbox,
+                element: "#sucury_waf_cache_sync",
+                target: "label[for=sucury_waf_cache_sync]"
             }
         }
     }
