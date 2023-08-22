ff11591c2ecfe8279a35eee7411bd35ae594eba6
COQUARD Cyrille <coquardcyr@gmail.com>
2023-08-22 14:27:38 +0200

    Fixed CR

--- src/support/hooks.ts
+++ src/support/hooks.ts
@@ -6,8 +6,6 @@ import { PageUtils } from "../../utils/page-utils";
 
 import { After, AfterAll, Before, BeforeAll, Status, setDefaultTimeout } from "@cucumber/cucumber";
 import fs from "fs/promises";
-// import { deleteTransient, resetWP } from "../../utils/commands";
-import { WP_BASE_URL } from "../../config/wp.config";
 import wp, {cp, deleteTransient, generateUsers, resetWP, rm, unzip} from "../../utils/commands";
 import {configurations, getWPDir} from "../../utils/configurations";
 
@@ -74,7 +72,7 @@ Before(async function (this: ICustomWorld) {
     this.sections = new Sections(this.page, pluginSelectors);
     this.utils = new PageUtils(this.page, this.sections);
     
-    await this.page.goto(WP_BASE_URL);
+    await this.page.goto(configurations.baseUrl);
 
 });
 
