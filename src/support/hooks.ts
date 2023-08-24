1a48f432143264b35c2b23a9ecf1cf6910a43b62
Michael Lee <michaelleemichaellee408@gmail.com>
2023-08-24 08:07:23 +0100

    Comment cli related code

--- src/support/hooks.ts
+++ src/support/hooks.ts
@@ -6,60 +6,63 @@ import { PageUtils } from "../../utils/page-utils";
 
 import { After, AfterAll, Before, BeforeAll, Status, setDefaultTimeout } from "@cucumber/cucumber";
 import fs from "fs/promises";
-import wp, {cp, deleteTransient, generateUsers, resetWP, rm, unzip} from "../../utils/commands";
-import {configurations, getWPDir} from "../../utils/configurations";
+// import wp, {cp, deleteTransient, generateUsers, resetWP, rm, unzip} from "../../utils/commands";
+// import {configurations, getWPDir} from "../../utils/configurations";
 
 let browser: ChromiumBrowser;
 
 setDefaultTimeout(process.env.PWDEBUG ? -1 : 60 * 10000);
 
-BeforeAll(async function () {
-    browser = await chromium.launch({ headless: false });
+BeforeAll(async function (this: ICustomWorld) {
+    browser = await chromium.launch({ headless: true });
 });
 
 Before(async function (this: ICustomWorld) {
 
-    await resetWP();
-    const wpDir = getWPDir(configurations);
-    await rm(`${wpDir}/wp-content/plugins/wp-rocket`)
-    await rm(`${wpDir}/wp-content/plugins/wp-rocket-e2e-test-helper`)
-    await wp('rewrite structure /%year%/%monthnum%/%postname%/')
+    /**
+     * To uncomment during implementation of cli
+     */
+    // await resetWP();
+    // const wpDir = getWPDir(configurations);
+    // await rm(`${wpDir}/wp-content/plugins/wp-rocket`)
+    // await rm(`${wpDir}/wp-content/plugins/wp-rocket-e2e-test-helper`)
+    // await wp('rewrite structure /%year%/%monthnum%/%postname%/')
 
-    await cp(`${process.env.PWD}/plugin/wp-rocket.zip`, `${wpDir}/wp-content/plugins/wp-rocket.zip`)
-    await unzip(`${wpDir}/wp-content/plugins/wp-rocket.zip`, `${wpDir}/wp-content/plugins/`)
-    await rm(`${wpDir}/wp-content/plugins/wp-rocket.zip`)
+    // await cp(`${process.env.PWD}/plugin/wp-rocket.zip`, `${wpDir}/wp-content/plugins/wp-rocket.zip`)
+    // await unzip(`${wpDir}/wp-content/plugins/wp-rocket.zip`, `${wpDir}/wp-content/plugins/`)
+    // await rm(`${wpDir}/wp-content/plugins/wp-rocket.zip`)
 
-    await cp(`${process.env.PWD}/plugin/wp-rocket-e2e-test-helper.zip`, `${wpDir}/wp-content/plugins/wp-rocket-e2e-test-helper.zip`)
-    await unzip(`${wpDir}/wp-content/plugins/wp-rocket-e2e-test-helper.zip`, `${wpDir}/wp-content/plugins/`)
-    await rm(`${wpDir}/wp-content/plugins/wp-rocket-e2e-test-helper.zip`)
+    // await cp(`${process.env.PWD}/plugin/wp-rocket-e2e-test-helper.zip`, `${wpDir}/wp-content/plugins/wp-rocket-e2e-test-helper.zip`)
+    // await unzip(`${wpDir}/wp-content/plugins/wp-rocket-e2e-test-helper.zip`, `${wpDir}/wp-content/plugins/`)
+    // await rm(`${wpDir}/wp-content/plugins/wp-rocket-e2e-test-helper.zip`)
 
-    await generateUsers([
-        {
-            name: 'admin2',
-            email: 'administrator@email.org',
-            role: 'administrator',
-        },
-        {
-            name: 'subscriber',
-            email: 'subscriber@email.org',
-            role: 'subscriber',
-        },
-        {
-            name: 'editor',
-            email: 'editor@email.org',
-            role: 'editor',
-        },
-        {
-            name: 'author',
-            email: 'author@email.org',
-            role: 'author',
-        },
-        {
-            name: 'contributor',
-            email: 'contributor@email.org',
-            role: 'contributor',
-        },
-    ])
+    // await generateUsers([
+    //     {
+    //         name: 'admin2',
+    //         email: 'administrator@email.org',
+    //         role: 'administrator',
+    //     },
+    //     {
+    //         name: 'subscriber',
+    //         email: 'subscriber@email.org',
+    //         role: 'subscriber',
+    //     },
+    //     {
+    //         name: 'editor',
+    //         email: 'editor@email.org',
+    //         role: 'editor',
+    //     },
+    //     {
+    //         name: 'author',
+    //         email: 'author@email.org',
+    //         role: 'author',
+    //     },
+    //     {
+    //         name: 'contributor',
+    //         email: 'contributor@email.org',
+    //         role: 'contributor',
+    //     },
+    // ])
 
 
     this.context = await browser.newContext({
@@ -72,7 +75,10 @@ Before(async function (this: ICustomWorld) {
     this.sections = new Sections(this.page, pluginSelectors);
     this.utils = new PageUtils(this.page, this.sections);
     
-    await this.page.goto(configurations.baseUrl);
+    /**
+     * To uncomment during implementation of cli
+     */
+    // await this.page.goto(configurations.baseUrl);
 
 });
 
@@ -98,13 +104,16 @@ After(async function (this: ICustomWorld, { pickle, result }) {
         );
     }
 
-     await resetWP();
+    //  await resetWP();
 
 });
 
- After(async function () {
-     deleteTransient('wp_rocket_customer_data')
- })
+/**
+ * To uncomment during implementation of cli
+ */
+//  After(async function () {
+//      deleteTransient('wp_rocket_customer_data')
+//  })
 
 AfterAll(async function () {
     await browser.close();
