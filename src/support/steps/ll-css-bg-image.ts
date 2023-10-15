838821a6f150f86e4c0bd4f651b1ca8e2b6d66ba
Michael Lee <michaelleemichaellee408@gmail.com>
2023-10-15 22:30:57 +0100

    Added step to assert correct background styles are loaded in the head

--- src/support/steps/ll-css-bg-image.ts
+++ src/support/steps/ll-css-bg-image.ts
@@ -33,3 +33,54 @@ Then('no error in the console different than nowprocket page', async function (t
 
       expect(consoleMsg1 === consoleMsg2).toBeTruthy();
 });
+
+Then('I must see the correct style in the head.', async function (this: ICustomWorld) {
+  const html = await this.page.evaluate(async () => {
+    // Scroll to the bottom of page.
+    const scrollPage: Promise<void> = new Promise((resolve) => {
+        let totalHeight = 0;
+        const distance = 100;
+        const timer = setInterval(() => {
+        const scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+
+        if(totalHeight >= scrollHeight){
+            clearInterval(timer);
+            resolve();
+        }
+        }, 500);
+    });
+
+    await scrollPage;
+
+    const styleTag = document.querySelector('#wpr-lazyload-bg');
+    const match = document.documentElement.outerHTML.match(/const rocket_pairs = \[(.*?)\];/);
+
+    return [
+      styleTag ? styleTag.textContent : null,
+      match ? match[1] : null
+    ];
+  });
+
+  const styles = html[0];
+  const rocketPairs = html[1];
+
+  const regex = /:root[^}]+}/g;
+  const matches = rocketPairs.match(regex);
+  
+  let isMatch = true;
+  if (matches) {
+    for (const match of matches) {
+      const unescapedString = match.replace(/\\\//g, '/');
+      if (!styles.includes(unescapedString)) {
+          isMatch = false;
+          break;
+      }
+    }
+  } else {
+    console.log("No matching style");
+  }
+
+  expect(isMatch).toBeTruthy();
+});
