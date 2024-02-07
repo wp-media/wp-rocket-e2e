2c8a19976a5edc2a3ddef831586fd41ec9e29cc5
Michael Lee <michaelleemichaellee408@gmail.com>
2024-02-07 20:29:57 +0100

    New delayjs step defintion

---
+++ src/support/steps/delay-js.ts
@@ -0,0 +1,23 @@
+/**
+ * @fileoverview
+ * This module contains Cucumber step definitions using Playwright for performing actions with Delayjs option enabled on WP Rocket.
+ * 
+ * 
+ *
+ * @requires {@link ../../common/custom-world}
+ * @requires {@link @cucumber/cucumber}
+ */
+import { ICustomWorld } from "../../common/custom-world";
+import { When } from '@cucumber/cucumber';
+
+/**
+ * Executes the step to move the mouse.
+ */
+When('move the mouse', async function (this: ICustomWorld) {
+    await this.page.mouse.down();
+    await this.page.mouse.up();
+});
+
+When('switch language', async function (this:ICustomWorld) {
+    await this.page.getByRole('link', { name: 'العربية (Arabic)' }).click();
+});
