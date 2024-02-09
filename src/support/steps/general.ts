85cdbd26bb8c96614845656b05e422b265b67608
Michael Lee <michaelleemichaellee408@gmail.com>
2024-02-09 01:37:31 +0100

    Updated to use regex

--- src/support/steps/general.ts
+++ src/support/steps/general.ts
@@ -315,5 +315,6 @@ const getConsoleMsg = async (page: Page, url: string): Promise<Array<string>> =>
  * Executes the step to assert that page navigation.
  */
 Then('page navigated to the new page {string}', async function (this: ICustomWorld, url) {
-    await expect(this.page).toHaveURL(`/${url}/`);
+    const regex = new RegExp(url);
+    await expect(this.page).toHaveURL(regex);
 });
