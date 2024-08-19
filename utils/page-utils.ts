c382d554902b7ca93a2482510026d75c161ed9d4
Mai Saad <mai@wp-media.me>
2024-08-19 21:54:35 +0300

    update logout to assert on notice instead of waitForLoadState

--- utils/page-utils.ts
+++ utils/page-utils.ts
@@ -283,10 +283,8 @@ export class PageUtils {
         await this.page.locator('#wp-admin-bar-my-account').hover();
         await this.page.waitForSelector('#wp-admin-bar-logout');
         await this.page.locator('#wp-admin-bar-logout a').click();
-  
-        await this.page.waitForLoadState('load', { timeout: 30000 });
-        await this.page.waitForTimeout(3000);
-    }
+        await expect(this.page.getByText('You are now logged out.')).toBeVisible();
+    }    
 
     /**
      * Performs Wordpress login action.
