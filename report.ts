05f07a9dc6e0e025724b7707c052e88eea593ebe
E2E Environment <e2e.report@wp-media.me>
2024-11-25 22:28:34 +0100

    Add shared directory

--- report.ts
+++ report.ts
@@ -4,79 +4,123 @@ import {exec} from "child_process";
 import { GITHUB_PAT} from "./config/wp.config";
 
 const GIT_PAT = GITHUB_PAT;
+
 if (!GIT_PAT) {
     console.error('Github PAT is not defined in the environment variables.');
     process.exit(1);
 }
 
 const remoteUrl = `https://${GIT_PAT}@github.com/wp-media/e2e_release_reports.git`;
-
-const testResults = path.join(__dirname, 'test-results');
+const sharedDirectory = '/var/shared/rocket-e2e-reports'
+const testResultsDir = path.join(__dirname, 'test-results');
 
 const args = process.argv.slice(2);
+
 if(args.length < 1) {
-    console.log(GIT_PAT);
     console.error('Please provide the new naming for test folder')
 }
-const newTestDir = args[0];
+const newTestResultsDir = args[0];
 
 const gitCommands = `
-    git checkout -b ${newTestDir};
+    git checkout -b ${newTestResultsDir};
     git add . && git commit -m "Test report";
-    git push origin test
+    git push -u origin ${newTestResultsDir}
 `;
 
+/**
+ * Setup git username and email
+ *
+ * @param {Function} callback - Function to execute after moving files.
+ */
+async function setGitGlobalConfig(callback: () => void): Promise<void>{
+    //Execute command to have username, defaulted it to e2e environment.
+    exec('git config --global user.name "E2E Environment" && git config --global user.email "e2e.report@wp-media.me"', (err) => {
+        if (err) {
+            console.error('Error setting global Git config:', err);
+            process.exit(1);
+        }
+        console.log('Global Git config set.');
+        callback();
+    });
+}
 
 //Check if .git directory exists
 export async function checkGitInitialized(callback) : Promise<void>{
-    const gitDir = path.join(newTestDir, '.git');
+    const gitDir = path.join(newTestResultsDir, '.git');
     if (!fs.existsSync(gitDir)) {
-        console.log('Git is not initialized. Setting it up...');
-        exec(`git init && git remote add origin ${remoteUrl}`, {cwd: newTestDir}, (initErr) => {
+        console.info('Git is not initialized. Setting it up...');
+        exec(`git init && git remote add origin ${remoteUrl}`, {cwd: newTestResultsDir }, (initErr) => {
             if (initErr) {
                 console.error('Error initializing Git:', initErr);
                 process.exit(1);
             }
-            console.log('Git initialized and remote added.');
+            console.info('Git initialized and remote added.');
             callback();
         });
 
-        //Execute command to have username, default it to e2e environment
-        exec('git config --global user.name "E2E Environment" && git config --global user.email "e2e.report@wp-media.me"', (err) => {
-            if (err) {
-                console.error('Error setting global Git config:', err);
-            } else {
-                console.log('Global Git config set.');
-            }
-        });
+        await setGitGlobalConfig(() => callback());
     } else {
         callback();
     }
 }
 
-// Rename test folder and push
-fs.rename(testResults, newTestDir, (err) => {
-    if (err) {
-        console.error('Error renaming/moving file:', err);
-    } else {
-        checkGitInitialized(() => {
-            exec(gitCommands, { cwd: newTestDir }, (err, stdout, stderr) => {
-                if (err) {
-                    console.error('Error executing git commands:', err);
-                }
+/**
+ * Moves all files and directories inside the test-results folder to the new directory.
+ * @param {string} testResultDir - The current test directory.
+ * @param {string} newSubDir - The new test result sub-directory.
+ * @param {Function} callback - Function to execute after moving files.
+ */
+export async function moveTestResultToSubdirectory(testResultDir, newSubDir, callback): Promise<void> {
+    const newTestDir = path.join(sharedDirectory, newSubDir);
 
-                if (stderr) {
-                    console.error('Git stderr output:', stderr);
-                    return;
-                }
-
-                //Revert the renaming of test folder to avoid tracking it in e2e
-                fs.rename(newTestDir, testResults, (err) => {
-                    if (err) {
-                        console.error('Error reverting the directory name:', err);
-                    }
-                });
-            });
-        })
+    // Main Process
+    if (!fs.existsSync(newTestDir)) {
+        fs.mkdirSync(newTestDir, { recursive: true });
     }
+
+    fs.readdir(testResultDir, (err, items) => {
+        if (err) {
+            console.error('Error reading directory:', err);
+            process.exit(1);
+        }
+
+        items.forEach(item => {
+            const itemPath = path.join(testResultsDir, item);
+
+            // Skip the newly created directory
+            if (item === newSubDir) return;
+
+            const destPath = path.join(newTestDir, item);
+
+            // Move the file or directory
+            fs.rename(itemPath, destPath, err => {
+                if (err) {
+                    console.error(`Error moving "${item}" to "archive":`, err);
+                } else {
+                    console.log(`Moved ${item} to ${newTestDir} successfully.`);
+                }
+            });
+        });
+
+        callback()
+    });
+}
+
+export async function executeGitCommands() :Promise<void> {
+    exec(gitCommands, (err, stdout, stderr) => {
+        if (err) {
+            console.error('Error executing git commands:', err);
+        }
+
+        if (stderr) {
+            console.error('Git stderr output:', stderr);
+        } else {
+            console.log('Git commands executed successfully:', stdout);
+        }
+    });
+}
+
+
+moveTestResultToSubdirectory(testResultsDir, newTestResultsDir, async () => {
+    //await checkGitInitialized(async () => await executeGitCommands());
 });
