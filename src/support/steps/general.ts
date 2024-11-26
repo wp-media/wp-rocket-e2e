a82e6826a92a491ab4f7283cc918733dc9de14b2
Michael Lee <michaelleemichaellee408@gmail.com>
2024-11-26 17:56:47 +0100

    Generate reference just once for same tag

--- src/support/steps/general.ts
+++ src/support/steps/general.ts
@@ -117,6 +117,11 @@ Given('visual regression reference is generated', async function (this:ICustomWo
     const tags = this.pickle.tags.map(tag => tag.name);
     const tag: string = await getScenarioTag(tags);
 
+    // Bail out if there is already reference for the current tag.
+    if (process.env.scenario_tag === tag) {
+        return;
+    }
+
     try {
         await batchUpdateVRTestUrl({
             optimize: false,
@@ -131,6 +136,8 @@ Given('visual regression reference is generated', async function (this:ICustomWo
     } catch (error) {
         console.error('Backstop reference generation failed: ', error.message);
     }
+
+    process.env.scenario_tag = tag;
 });
 
 /**
