856d89720962df137fcb8ce44bc20df838e16152
Opeyemi Ibrahim <opeyemi.khadri@gmail.com>
2025-06-20 09:50:28 +0100

    Create BWU plugin, installation and basic onboarding step (#221)
    * Create BWU plugin, installation and basic onboarding step
    
    * Delete plugin by ui since wp cli does not delete data
    
    * pr review modifications
    
    * modify cucumber file
    
    * Move bwup settings to a diff directory
    
    * fix lint
    
    * fix lint
    
    * remove unused logic
    
    * Modify file structure

---
+++ src/backwpup/steps/delete.ts
@@ -0,0 +1,33 @@
+/**
+ * @fileoverview
+ * This module contains Cucumber step definitions using Playwright for deleting the WP Rocket plugin.
+ * It includes steps for confirming the deletion, navigating to the plugins page, deactivating the plugin,
+ * handling deactivation modal, initiating the deletion process, and asserting successful deletion.
+ *
+ * @requires {@link ../../common/custom-world}
+ * @requires {@link @playwright/test}
+ * @requires {@link @cucumber/cucumber}
+ */
+import { ICustomWorld } from "../../common/custom-world";
+import { When } from '@cucumber/cucumber';
+
+/**
+ * Executes the step to delete the WP Rocket plugin.
+ */
+When('I delete backwpup plugin', async function (this: ICustomWorld) {
+
+    // Goto plugins page.
+    await this.utils.gotoPlugin();
+
+    // Ensure WPR is deactivated.
+    await this.utils.togglePluginActivation('backwpup-pro', false);
+
+    // Check for deactivation modal.
+    if (await this.page.locator('label[for=deactivate]').isVisible()) {
+        await this.page.locator('label[for=deactivate]').click();
+        await this.page.locator('text=Confirm').click();
+    }
+
+    // Delete WPR.
+    await this.utils.removeBackWpViaUi();
+});
---
+++ src/backwpup/support/hooks.ts
@@ -0,0 +1,18 @@
+import { ICustomWorld } from "../../common/custom-world";
+import { Sections } from '../../common/sections';
+import { selectors as pluginSelectors } from "./../../common/selectors";
+import { PageUtils } from "../../../utils/page-utils";
+import { Before } from "@cucumber/cucumber";
+
+
+/**
+ * Before each test scenario with the @bwpsetup tag, performs setup tasks.
+ */
+Before({tags: '@bwpsetup'}, async function(this: ICustomWorld, {pickle}) {
+
+    this.page = await this.context.newPage();
+    this.sections = new Sections(this.page, pluginSelectors);
+    this.utils = new PageUtils(this.page, this.sections);
+
+    this.pickle = pickle;
+});
