e822646ddc85c762983f391502074ac83608a03d
Sandy Figueroa <sandyfigueroa@users.noreply.github.com>
2025-06-24 18:04:58 -0500

    Added download-restore feature

--- src/backwpup/features/download-restore.feature
+++ src/backwpup/features/download-restore.feature
@@ -1,18 +1,12 @@
 @bwpsetup @bwpdownloadrestore @bwupsmoke
 Feature: BackWpUp Download and Restore
 
-    Background:
+    Scenario: Should download backup
         Given I am logged in
         And I delete backwpup plugin
         And plugin is installed 'backwpup-pro'
         And plugin is activated
-
-    Scenario: Should download backup
-        When I go '/wp-admin/admin.php?page=backwpup'
-        And I click '.js-backwpup-onboarding-step-2' button to continue
-        And I click '.js-backwpup-onboarding-step-3' button to continue
-        When I Configure web server storage
-        And I go '/wp-admin/admin.php?page=backwpup'
+        Given First backup generated with default settings and local storage
         Then I should see 'mixed' job cards
         And I successfully download first backup in the history
 
--- src/backwpup/steps/backup-history.ts
+++ src/backwpup/steps/backup-history.ts
@@ -1,6 +1,8 @@
 import { Then } from '@cucumber/cucumber';
 import { ICustomWorld } from '../../common/custom-world';
 import { expect } from '@playwright/test';
+import * as path from 'path';
+import { promises as fs } from 'fs';
 
 /**
  * Click on download button for the first backup in the history
@@ -8,18 +10,28 @@ import { expect } from '@playwright/test';
 Then(
     'I successfully download first backup in the history',
     async function (this: ICustomWorld) {
-        const [download] = await Promise.all([
-            this.page.waitForEvent('download'), // wait for download to start
-            this.page
-                .locator('button.js-backwpup-download-backup')
-                .first()
-                .click() // click the button to start download
-        ]);
+        const openMenuButton = '.js-backwpup-menu';
+        const downloadButton =
+            '.js-backwpup-menu-content>button.js-backwpup-download-backup';
+        const downloadModal = '#TB_window';
+        await this.page.locator(openMenuButton).waitFor({ state: 'visible' });
+        await this.page.locator(openMenuButton).first().click(); // click to open the menu
+        await this.page.locator(downloadButton).waitFor({ state: 'visible' });
+        await this.page.locator(downloadButton).first().click(); // click the button to start download
+        const downloadPromise = this.page.waitForEvent('download', {timeout: 60000}); // wait for the download event
+        await this.page.locator(downloadModal).waitFor({ state: 'visible' });
+        const download = await downloadPromise;
         const filename = download.suggestedFilename();
         expect(filename).toBeTruthy(); // non-empty string
 
-        const savePath = ''; // TODO: specify the path where you want to save the downloaded file
+        // Ensure the `temp` directory exists at project root
+        const tempDir = path.resolve(process.cwd(), 'temp');
+        await fs.mkdir(tempDir, { recursive: true });
+        const savePath = path.join(tempDir, filename);
         await download.saveAs(savePath);
         expect(await download.path()).toBeTruthy(); // path is not null or undefined
+
+        // Clear up the downloaded file after the test
+        await fs.rm(savePath, { force: true });
     }
 );
