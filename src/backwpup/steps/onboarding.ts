f1b4ecde360d6adb0b1de9e82a0c474010ec25d4
Sandy Figueroa <sandyfigueroa@users.noreply.github.com>
2025-06-24 16:09:27 -0500

    Abstracted Web Server storage configuration

--- src/backwpup/steps/onboarding.ts
+++ src/backwpup/steps/onboarding.ts
@@ -1,7 +1,7 @@
 import {Then, When} from "@cucumber/cucumber";
 import {ICustomWorld} from "../../common/custom-world";
 import {expect} from "@playwright/test";
-import { clickContinueButton } from "../utils/helpers";
+import { clickContinueButton, configureWebServerStorage } from "../utils/helpers";
 
 /**
  * Click on save and continue button during onboarding.
@@ -12,20 +12,7 @@ When('I click {string} button to continue', async function (this: ICustomWorld, 
 });
 
 When('I Configure web server storage', async function (this: ICustomWorld) {
-    await this.page.locator('.js-backwpup-toggle-storage').first().click();
-    await this.page.click('.js-backwpup-test-FOLDER-storage')
-
-    //Save and submit onboarding form
-    await this.page.click('.js-backwpup-onboarding-submit-form');
-
-    await this.page.waitForTimeout(60000);
-
-    const closeButton = '#showworkingclose'
-    await this.page.waitForSelector(closeButton, {
-        state: 'visible',
-        timeout: 10000
-    });
-    await this.page.click(closeButton)
+    await configureWebServerStorage(this.page);
 });
 
 
--- src/backwpup/utils/helpers.ts
+++ src/backwpup/utils/helpers.ts
@@ -5,7 +5,6 @@ import { ICustomWorld } from "../../common/custom-world";
  *
  * @param page - The Playwright page instance used for interaction.
  * @param button - A selector string identifying the button element to click.
- * @throws Will throw an error if the button is not visible within the 10-second timeout.
  * @returns A promise that resolves once the click action is completed.
  */
 export const clickContinueButton = async (page: ICustomWorld['page'], button: string): Promise<void> => {
@@ -15,4 +14,27 @@ export const clickContinueButton = async (page: ICustomWorld['page'], button: st
     });
 
     return await page.click(button);
+};
+
+/**
+ * Configures the web server storage in the onboarding process
+ *
+ * @param page - The Playwright Page instance used to perform UI interactions.
+ * @returns A promise that resolves when the web server storage configuration is fully applied.
+ */
+export const configureWebServerStorage = async (page: ICustomWorld['page']): Promise<void> => {
+        await page.locator('.js-backwpup-toggle-storage').first().click();
+        await page.click('.js-backwpup-test-FOLDER-storage')
+    
+        //Save and submit onboarding form
+        await page.click('.js-backwpup-onboarding-submit-form');
+    
+        await page.waitForTimeout(60000);
+    
+        const closeButton = '#showworkingclose'
+        await page.waitForSelector(closeButton, {
+            state: 'visible',
+            timeout: 10000
+        });
+        await page.click(closeButton);
 };
