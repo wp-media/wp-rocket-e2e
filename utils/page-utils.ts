bd19b5bf09c1ddf3be2f5ccb71be2005ab99c223
Mai <mai@wp-media.me>
2025-11-23 09:44:10 +0200

    verify email only if related text popup is there

--- utils/page-utils.ts
+++ utils/page-utils.ts
@@ -94,10 +94,9 @@ export class PageUtils {
         await this.page.click('#wp-submit');
 
         // Check if admin email confirmation form is displayed
-        if (await this.page.locator('#confirm_admin_email_nonce').isVisible()) {
+        if (await this.page.getByText('Administration email verification').isVisible()) {
             await this.page.locator('#correct-admin-email').click();
         }
-
 
         // Confirm login worked
         await this.page.waitForURL('**/wp-admin/**', { timeout: 5000 });
