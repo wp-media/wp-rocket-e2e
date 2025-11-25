30c789dace066f904fe1de52299e20b9aad0eec9
Mai <mai@wp-media.me>
2025-11-25 11:54:49 +0200

    removed code of other PR

--- src/support/steps/general.ts
+++ src/support/steps/general.ts
@@ -16,7 +16,7 @@ import { ICustomWorld } from "../../common/custom-world";
 import { Given, When, Then } from '@cucumber/cucumber';
 import {WP_BASE_URL} from '../../../config/wp.config';
 import scenarioUrls from "./../../../config/scenarioUrls.json";
-import { compareReference, isTagPresent, getScenarioTag, batchUpdateVRTestUrl, setHelperFilter } from "../../../utils/helpers";
+import { compareReference, isTagPresent, getScenarioTag, batchUpdateVRTestUrl} from "../../../utils/helpers";
 import type { Section } from "../../../utils/types";
 import { Page } from '@playwright/test';
 import {
@@ -29,7 +29,6 @@ import backstop from 'backstopjs';
  */
 Given('I am logged in', async function (this: ICustomWorld) {
     await this.utils.auth();
- //   await setHelperFilter(this.page, 'rocket_rocket_insights_enabled', 'false');
 });
 
 /**
